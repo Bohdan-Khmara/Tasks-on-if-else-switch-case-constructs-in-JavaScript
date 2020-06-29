88ed63ef559e4480c5dc172ab9aa91b6403f062d
Bohdan Khmara <khmarabohdan@gmail.com>
2020-06-29 19:12:03 +0300

    Switch-case. Task 4

--- index.js
+++ index.js
@@ -1,30 +1,16 @@
 /**
- * Switch-case. Task 3
+ * Switch-case. Task 4
  * @type {boolean}
  */
-let month = Number(prompt('Введите число от 1 до 12'));
-let result;
+let string = prompt('Введите строку');
+let result = string.split('');
 
-switch (month) {
-    case 1:
-    case 2:
-    case 3:
-        alert('зима');
-        break;
-
-    case 4:
-    case 5:
-    case 6:
-        alert('весна');
-        break;
-
-    case 7:
-    case 8:
-    case 9:
-        alert('лето');
+switch (result[0]) {
+    case 'a':
+        alert('да');
         break;
 
     default:
-        alert('осень');
+        alert('нет');
         break;
 };
