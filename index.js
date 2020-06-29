990bc05173110508f4d862f1b81815f4c1dfe482
Bohdan Khmara <Khmarabohdan@gmail.com>
2020-06-29 19:16:19 +0300

    Switch-case. Task 7

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 /**
- * Switch-case. Task 6
+ * Switch-case. Task 7
  * @type {boolean}
  */
 let string = prompt('Введите строку');
 let splitStr = string.split('');
-let firstNumb = Number(splitStr[0]);
-let secondNumb = Number(splitStr[1]);
-let thirdNumb = Number(splitStr[2]);
+let value1 = Number(splitStr[0]);
+let value2 = Number(splitStr[1]);
+let value3 = Number(splitStr[2]);
+let value4 = Number(splitStr[3]);
+let value5 = Number(splitStr[4]);
+let value6 = Number(splitStr[5]);
+let sum1 = value1 + value2 + value3;
+let sum2 = value4 + value5 + value6;
 
-switch (firstNumb, secondNumb, thirdNumb) {
+switch (sum1, sum2) {
     default:
-        firstNumb + secondNumb + thirdNumb;
+        let result = (sum1 === sum2) ? alert('да') : alert('нет');
         break;
 };
