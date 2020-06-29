1782ee4051bcb266004a16a4096980f019c1a9f4
Bohdan Khmara <Khmarabohdan@gmail.com>
2020-06-29 18:40:57 +0300

    Work with if-else. Task 6

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 /**
- * Work with if-else. Task 5
+ * Work with if-else. Task 6
  * @type {number}
  */
-let a = 0;
+let a = 1;
 
-if (a <= 0) {
+if (a !== 0) {
     alert('Верно!');
 } else alert('Не верно!');
